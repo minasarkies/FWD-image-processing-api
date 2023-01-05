85d30c4f808e5bfc8f63b5568ea82fdde7e23f38
narouz <mina.sarkies1995@gmail.com>
2023-01-05 17:04:30 +0400

    finalized setup and added code files

--- src/tests/helpers/reporter.ts
+++ src/tests/helpers/reporter.ts
@@ -1,4 +1,8 @@
-import { DisplayProcessor, SpecReporter, StacktraceOption } from 'jasmine-spec-reporter'
+import {
+  DisplayProcessor,
+  SpecReporter,
+  StacktraceOption,
+} from 'jasmine-spec-reporter'
 import SuiteInfo = jasmine.SuiteInfo
 class CustomProcessor extends DisplayProcessor {
   public displayJasmineStarted(info: SuiteInfo, log: string): string {
@@ -9,8 +13,8 @@ jasmine.getEnv().clearReporters()
 jasmine.getEnv().addReporter(
   new SpecReporter({
     spec: {
-      displayStacktrace: StacktraceOption.NONE
+      displayStacktrace: StacktraceOption.NONE,
     },
-    customProcessors: [CustomProcessor]
+    customProcessors: [CustomProcessor],
   }) as CustomReporter
 )
