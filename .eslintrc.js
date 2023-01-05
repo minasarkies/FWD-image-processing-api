6bd1e990d7e75f2f725c1757fce09a03c2e498b1
narouz <mina.sarkies1995@gmail.com>
2023-01-05 16:39:48 +0400

    initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "env": {
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+        semi: ['error', 'never'],
+        quotes: ['error', 'single'],
+        'prettier/prettier': 2, // Means error
+        'no-console': 1, // Means warning
+        'no-var': 'error',
+        'prefer-const': 'error'
+    }
+}
